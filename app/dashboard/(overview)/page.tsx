06ffeab1cc90220b1010015da96c7a5eb05d7ada
shirik78 <65509343+shirik78@users.noreply.github.com>
2024-06-26 11:01:11 +0300

    chapter#16 Done!

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import {Metadata} from 'next';
+
+export const metadata:Metadata = {
+    title:'Dashboard'
+}
  
 export default async function Page() {
     
